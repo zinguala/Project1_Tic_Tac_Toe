ee4fbc11acd26f27ed1aa1c3220715ce6cb1d8bc
Aviv-PC <aviv.zinger@gmail.com>
2023-03-18 20:00:50 +0200

    all checks ok menu 1 ready need to change: menu 2 menu 3

--- main.py
+++ main.py
@@ -35,15 +35,21 @@ def win_check(args, player_help=0):  # function for win check and option to help
                 if count_row_x == count_for_win:  # check for win in row
                     return 'x'
                 if player_help == 1 and count_row_x == (count_for_win - 1):
-                    print(f'Player 1 next move for win choose square: {i}', end="")
-                    print(args[i].index('_'))
+                    try:
+                        print(f'Player 1 next move for win choose square: {i}', args[i].index('_'), sep='')
+                    except ValueError:
+                        print('')
+
             elif args[i][k] == 'o':
                 count_row_o += 1  # count 1 up when there is o in this place
                 if count_row_o == count_for_win:  # see if the o counter in row reached to the limit for win
                     return 'o'
                 if player_help == 2 and count_row_o == (count_for_win - 1):
-                    print(f'Player 2 next move for win choose square: {i}', end="")
-                    print(args[i].index('_'))
+
+                    try:
+                        print(f'Player 2 next move for win choose square: {i}', args[i].index('_'), sep='')
+                    except ValueError:
+                        print('')
             if args[k][i] == 'x':  # lines check for win
                 count_line_x += 1  # count 1 up when there is x in this place
                 if count_line_x == count_for_win:  # see if the x counter in line reached to the limit for win
@@ -70,7 +76,7 @@ def win_check(args, player_help=0):  # function for win check and option to help
                     if count_first_diagonal_o == count_for_win:
                         return 'o'
 
-    if player_help == 1 and count_first_diagonal_x == (count_for_win - 1):  # help for player 1 diagonal
+    if player_help == 1 and count_first_diagonal_x == (count_for_win - 1):  # help for player 1 first diagonal
         for v in range(count_for_win):
             if args[v][v] == '_':
                 print(f'Player 1 next move for win choose square: {v}{v}')
@@ -89,6 +95,17 @@ def win_check(args, player_help=0):  # function for win check and option to help
             count_second_diagonal_o += 1
             if count_second_diagonal_o == count_for_win:
                 return 'o'
+
+    if player_help == 1 and count_second_diagonal_x == (count_for_win - 1):
+        for r in range(len(args)):   # second diagonal check for the
+            if args[count_for_win - r - 1][r] == '_':  # check the second diagonal for the
+                print(f'Player 2 next move for win choose square: {count_for_win - r - 1}{r}')
+
+    if player_help == 2 and count_second_diagonal_o == (count_for_win - 1):
+        for n in range(len(args)):   # second diagonal check for the
+            if args[count_for_win - n - 1][n] == '_':  # check the second diagonal for the
+                print(f'Player 2 next move for win choose square: {count_for_win - n - 1}{n}')
+
     return None
 
 
@@ -108,6 +125,7 @@ while Program_Run:
     player2_help = ''
     counter_for_draw = 0
     Player = 0
+    replay = ''
     print('Welcome noughts and crosses :)\n \nTo Choose which game you want to play press: \n 1-player vs player\n'
           ' 2-player vs computer\n 3-computer vs computer\n 4-exit\n')
     try:
@@ -117,116 +135,128 @@ while Program_Run:
         continue
 
     if Menu == 1:
-        print('welcome to player vs player \n')
         while True:
-            player1_help = input('Player 1 Do you want help during the game? y/n : ')
-            if player1_help in ['y', 'n']:
-                print(f'user typed {player1_help}')
-                break
-            else:
-                print('Type y or n')
-                continue
-        while True:
-            player2_help = input('Player 2 Do you want help during the game? y/n : ')
-            if player2_help in ['y', 'n']:
-                print(f'user typed {player1_help}')
-                break
-            else:
-                print('Type y or n')
-                continue
-
-        while True:  # while for checking the input of the size of the game board, so it will be 3 or 4 only
-            try:
-                Board_size = int(input('please select board size 3 or 4 : '))  # player can choose the size of the board
-            except ValueError:
-                continue
-            if Board_size == 3:
-                print(f'You choose size: {Board_size}x{Board_size}\n')
-                Game_Board = [['_', '_', '_'], ['_', '_', '_'], ['_', '_', '_']]
-                Game_map = Game_map_size3
-                break
-            if Board_size == 4:
-                print(f'You choose size: {Board_size}x{Board_size}\n')
-                Game_Board = [['_', '_', '_', '_'], ['_', '_', '_', '_'], ['_', '_', '_', '_'], ['_', '_', '_', '_']]
-                Game_map = Game_map_size4
-                break
-        current(Game_Board)
-        current(Game_map)
-        print('first player you choose where to place -x\nsecond player you choose where to place -o\n ')
-        while True:  # while that running the initial game
-            print('Player 1 your turn, please choose where to put-x \n')
-            while True:  # while for player 1 turn
-                try:
-                    Row = int(input("first Enter row : "))
-                except ValueError:
-                    continue
-                try:
-                    Line = int(input("now Enter line : "))
-                except ValueError:
-                    continue
-                if (Row not in range(len(Game_Board))) or (
-                        Line not in range(len(Game_Board))):  # check coordinates if in range
-                    print(f'please enter valid number between 0 - {(len(Game_Board) - 1)}\n')
-                elif Game_Board[Row][Line] != '_':  # check if this square is already taken
-                    print(f'this square {Row}{Line} is already taken please choose other one')
-                else:
-                    Game_Board[Row][Line] = 'x'  # insert the symbol of the player in the coordinates chose
-                    counter_for_draw += 1
-                    break
-
-            current(Game_Board)  # calls the current function to show the board
-            current(Game_map)  # calls the current function to show the map
-
-            if 'y' in player2_help:  # if for help player 2
-                if win_check(Game_Board) == 'x':  # winner check with win_check function
-                    print('player 1 you are the winner!!!!!!!!!!!!!!!\n')
+            print('welcome to player vs player \n')
+            while True:
+                player1_help = input('Player 1 Do you want help during the game? y/n : ')
+                if player1_help in ['y', 'n']:
+                    print(f'user typed {player1_help}')
                     break
                 else:
-                    win_check(Game_Board, 2)
-
-            if counter_for_draw == (len(Game_Board) ** 2):  # draw check if now squares left
-                print('Its a Draw no one wins :)\n')
-                break
-
-            print('Player 2 your turn, please choose where to put-o \n')
-
-            while True:  # while for turn of player 2
-                try:
-                    Row = int(input("first Enter row : "))  # coordinates choose
-                except ValueError:
+                    print('Type y or n')
                     continue
-                try:
-                    Line = int(input("now Enter line : "))  # coordinates choose
-                except ValueError:
-                    continue
-                if (Row not in range(len(Game_Board))) or (
-                        Line not in range(len(Game_Board))):  # check coordinates if in range
-                    print(f'please enter valid number between 0 - {(len(Game_Board) - 1)}\n')
-                elif Game_Board[Row][Line] != '_':  # check if this square is already taken
-                    print(f'this square {Row}{Line} is already taken please choose other one\n')
-                else:
-                    Game_Board[Row][Line] = 'o'  # insert the symbol of the player in the coordinates chose
-                    counter_for_draw += 1
-                    break
-
-            current(Game_Board)  # calls the current function to show the board
-            current(Game_map)  # calls the current function to show the map
-
-            if 'y' in player1_help:
-                if win_check(Game_Board) == 'o':  # winner check with win_check function
-                    print('player 2 you are the winner!!!!!!!!!!!!!!!\n')  # check if player 2 is the winner
+            while True:
+                player2_help = input('Player 2 Do you want help during the game? y/n : ')
+                if player2_help in ['y', 'n']:
+                    print(f'user typed {player1_help}')
                     break
                 else:
-                    win_check(Game_Board, 1)  #
+                    print('Type y or n')
+                    continue
 
-            elif win_check(Game_Board) == 'o':  # winner check with win_check function
-                print('player 2 you are the winner!!!!!!!!!!!!!!!\n')
+            while True:  # while for checking the input of the size of the game board, so it will be 3 or 4 only
+                try:
+                    Board_size = int(input('please select board size 3 or 4 : '))  # player can choose the size of board
+                except ValueError:
+                    continue
+                if Board_size == 3:
+                    print(f'You choose size: {Board_size}x{Board_size}\n')
+                    Game_Board = [['_', '_', '_'], ['_', '_', '_'], ['_', '_', '_']]
+                    Game_map = Game_map_size3
+                    break
+                if Board_size == 4:
+                    print(f'You choose size: {Board_size}x{Board_size}\n')
+                    Game_Board = [['_', '_', '_', '_'], ['_', '_', '_', '_'], ['_', '_', '_', '_'],
+                                  ['_', '_', '_', '_']]
+                    Game_map = Game_map_size4
+                    break
+            current(Game_Board)
+            current(Game_map)
+            print('first player you choose where to place -x\nsecond player you choose where to place -o\n ')
+            while True:  # while that running the initial game
+                print('Player 1 your turn, please choose where to put-x \n')
+                while True:  # while for player 1 turn
+                    try:
+                        Row = int(input("first Enter row : "))
+                    except ValueError:
+                        continue
+                    try:
+                        Line = int(input("now Enter line : "))
+                    except ValueError:
+                        continue
+                    if (Row not in range(len(Game_Board))) or (
+                            Line not in range(len(Game_Board))):  # check coordinates if in range
+                        print(f'please enter valid number between 0 - {(len(Game_Board) - 1)}\n')
+                    elif Game_Board[Row][Line] != '_':  # check if this square is already taken
+                        print(f'this square {Row}{Line} is already taken please choose other one')
+                    else:
+                        Game_Board[Row][Line] = 'x'  # insert the symbol of the player in the coordinates chose
+                        counter_for_draw += 1
+                        break
+
+                current(Game_Board)  # calls the current function to show the board
+                current(Game_map)  # calls the current function to show the map
+
+                if 'y' in player2_help:  # if for help player 2
+                    if win_check(Game_Board) == 'x':  # winner check with win_check function
+                        print('player 1 you are the winner!!!!!!!!!!!!!!!\n')
+                        break
+                    else:
+                        win_check(Game_Board, 2)
+
+                if counter_for_draw == (len(Game_Board) ** 2):  # draw check if now squares left
+                    print('Its a Draw no one wins :)\n')
+                    break
+
+                print('Player 2 your turn, please choose where to put-o \n')
+
+                while True:  # while for turn of player 2
+                    try:
+                        Row = int(input("first Enter row : "))  # coordinates choose
+                    except ValueError:
+                        continue
+                    try:
+                        Line = int(input("now Enter line : "))  # coordinates choose
+                    except ValueError:
+                        continue
+                    if (Row not in range(len(Game_Board))) or (
+                            Line not in range(len(Game_Board))):  # check coordinates if in range
+                        print(f'please enter valid number between 0 - {(len(Game_Board) - 1)}\n')
+                    elif Game_Board[Row][Line] != '_':  # check if this square is already taken
+                        print(f'this square {Row}{Line} is already taken please choose other one\n')
+                    else:
+                        Game_Board[Row][Line] = 'o'  # insert the symbol of the player in the coordinates chose
+                        counter_for_draw += 1
+                        break
+
+                current(Game_Board)  # calls the current function to show the board
+                current(Game_map)  # calls the current function to show the map
+
+                if 'y' in player1_help:
+                    if win_check(Game_Board) == 'o':  # winner check with win_check function
+                        print('player 2 you are the winner!!!!!!!!!!!!!!!\n')  # check if player 2 is the winner
+                        break
+                    else:
+                        win_check(Game_Board, 1)  #
+
+                elif win_check(Game_Board) == 'o':  # winner check with win_check function
+                    print('player 2 you are the winner!!!!!!!!!!!!!!!\n')
+                    break
+
+                if counter_for_draw == (len(Game_Board) ** 2):
+                    print('Its a Draw no one wins :)\n')
+                    break
+
+            while True:
+                replay = input('wanna replay? y/n : ')
+                if replay in ['y', 'n']:
+                    print(f'user typed {replay} returning to main menu\n')
+                    break
+                else:
+                    print('Type y or n')
+                    continue
+            if replay == 'n':
                 break
-
-            if counter_for_draw == (len(Game_Board) ** 2):
-                print('Its a Draw no one wins :)\n')
-                break
-
     elif Menu == 2:
         print('welcome to player vs computer \n')
         while True:  # while for checking the input of the size of the game board, so it will be 3 or 4 only
